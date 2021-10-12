bd70c1a36b8919424c413d1bd4125e626481a0da
riz-mbn <rizlee@mybizniche.com>
2021-10-12 10:13:58 +0800

    the catch page

--- templates/the-catch.php
+++ templates/the-catch.php
@@ -1,18 +1,46 @@
 <?php 
 /**
- * Template Name: WHat Is the Catch Page template
+ * Template Name: How It Works Page template
  */
 get_header() ?>
 
-<section class="page-content">
-	<div class="grid-container">		
-		<?php
-			while ( have_posts() ) : the_post();
-			the_title('<h1>', '</h1>');
-			the_content();
-
-		endwhile; // End of the loop.
-		?>
+<section class="the_catch">						
+	<div class="icon hide-for-large">
+		<figure><img src="<?php echo MBN_ASSETS_URI ?>/img/icon/icn-rect.svg" width="150" height="150" /></figure>
+	</div>
+	<div class="grid-container">	
+		<h1	class="title">What’s The Catch</h1>	
+		<p class="subtitle">Source Power Company’s have a deal that’s almost too good to be true. Almost.</p>
+			<div class="catch_boxes">
+				<div class="catch_box">
+					<div class="catch_item">
+						<figure><img src="<?php echo MBN_ASSETS_URI ?>/img/icon/icn-dollar.svg" width="100" height="100" /></figure>
+						<div class="text-wrap">
+							<h3>Folks can sign up for <span class="highlight">free</span>.</h3>
+							<p>There are <strong>no upfront costs, no credit check and enrollment</strong> is remarkably easy...and you start saving on Day One.</p>
+						</div>
+					</div>
+				</div>		
+				<div class="catch_box">
+					<div class="catch_item">
+						<figure><img src="<?php echo MBN_ASSETS_URI ?>/img/icon/icn-bottle-money.svg" width="83" height="102" /></figure>
+						<div class="text-wrap">
+							<h3>Nothing really changes except one thing:.</h3>
+							<p>You’ll see a VDER CREDIT that <strong>guarantees savings</strong> on all future electricity costs.  Every single month.</p>
+						</div>
+					</div>
+				</div>	
+				<div class="catch_box">
+					<div class="catch_item">
+						<figure><img src="<?php echo MBN_ASSETS_URI ?>/img/icon/icn-light-bulb.svg" width="115" height="100" /></figure>
+						<div class="text-wrap">
+							<h3><span class="highlight">green</span>, clean, renewable energy.</h3>
+							<p>You’re also supporting <strong>green, clean, renewable energy</strong>. And you’re creating local jobs.</p>
+						</div>
+					</div>
+				</div>
+			</div>		
+		</div>	
 	</div>	
 </section>
 
